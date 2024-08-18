1197fbd0fc9ad92a0a3167154c811647b3693268
AnupamSingh2004 <sanupam2004@gmail.com>
2024-08-18 18:55:13 +0530

    ProfilePage

--- Frontend/src/pages/Profile/profile.jsx
+++ Frontend/src/pages/Profile/profile.jsx
@@ -1,9 +1,17 @@
 import {useRecoilState} from "recoil";
 import {userState} from "@/store/atoms/userdata.js";
 import {IoArrowBack} from "react-icons/io5";
+import {Avatar} from "@radix-ui/react-avatar";
+import {AvatarImage} from "@/components/ui/avatar.jsx";
+import {useState} from "react";
+import {getColor} from "@/lib/utils.js";
 
 
 const Profile = () => {
+
+    const [firstName, setFirstName] = useState("");
+    const [image, setImage] = useState(null);
+    const [selectedColor, setSelectedColor] = useState(0);
 
 
     function logout() {
@@ -19,6 +27,22 @@ const Profile = () => {
         <div className={"flex flex-col gap-10 w-[80vw] md:w-max"}>
             <div>
                 <IoArrowBack className={"text-4xl lg:text-6xl text-white/90 cursor-pointer"}/>
+            </div>
+            <div className={"grid grid-cols-2"}>
+                <div className={"h-full w-32 md:w-48 md:h-48 relative flex items-center justify-center"}>
+
+                    <Avatar>
+                        {
+                            image ? (<AvatarImage/>) : (
+                                <div
+                                    className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex justify-center items-center rounded-full`}
+                                >
+                                    {/*{firstName ? firstName.split("").shift() :*/}
+                                    {/*    userData[0].displayName.split("").shift()}*/}
+                                </div>)
+                        }
+                    </Avatar>
+                </div>
             </div>
         </div>
     </div>
