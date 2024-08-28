a87bfff76ccc0dbde1075ca93a4de3518105fead
AnupamSingh2004 <sanupam2004@gmail.com>
2024-08-28 16:22:31 +0530

    Frontend changes

--- Frontend/src/pages/Chat/contacts-container/index.jsx
+++ Frontend/src/pages/Chat/contacts-container/index.jsx
@@ -1,9 +1,13 @@
 export const ContactsContainer = () => {
     return <div
         className={"relative md:w-[35vw] lg:w-[30vw] xl:w-[20vw] bg-[#1b1c24] border-r-2 border-[#2f303b]"}>
-        <Logo/>
-        <div className="">Hello</div>
+        <div className="pt-1">
+            <Logo/>
+        </div>
 
+        <div className="my-5">
+            <Title text={"Direct Messages"}/>
+        </div>
 
     </div>
 
@@ -16,5 +20,11 @@ const Logo = () => {
             <img src="../../../../public/Chatter.png" alt="Logo" className={"h-36 w-36 cursor-pointer"}/>
             <div className={"poppins-medium pt-14 text-4xl font-bold ml-[-10px] cursor-pointer"}>Chatters</div>
         </div>
+    )
+}
+
+const Title = ({text}) => {
+    return (
+        <h6 className={"uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90 text-md"}>{text}</h6>
     )
 }
